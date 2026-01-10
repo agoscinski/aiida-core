b9a19f106502c31bbaf05dc221ecf519bd19de35
Alexander Goscinski <alex.go@posteo.de>
2026-01-10 16:30:13 +0100

    Cleanup old code

--- src/aiida/brokers/namedpipe/__init__.py
+++ src/aiida/brokers/namedpipe/__init__.py
@@ -4,13 +4,11 @@ from .broker import PipeBroker
 from .broker_communicator import PipeBrokerCommunicator
 from .communicator import PipeCommunicator
 from .config import configure_profile_for_namedpipe, ensure_broker_running, is_namedpipe_broker
-from .message_broker import MessageBroker
 
 __all__ = (
     'PipeBroker',
     'PipeBrokerCommunicator',
     'PipeCommunicator',
-    'MessageBroker',
     'configure_profile_for_namedpipe',
     'ensure_broker_running',
     'is_namedpipe_broker',
