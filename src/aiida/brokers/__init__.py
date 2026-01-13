8dc2dfd0cb5e7a0d52bf286c8c4fceb8346b1cc9
Alexander Goscinski <alex.go@posteo.de>
2026-01-13 11:25:10 +0100

    Fix import bug

--- src/aiida/brokers/__init__.py
+++ src/aiida/brokers/__init__.py
@@ -5,14 +5,9 @@
 from .broker import *
 from .rabbitmq import *
 
-# For backward compatibility - executor is now in scheduler
-from aiida.engine.scheduler.executor import WorkerExecutor, WorkerInfo
-
 __all__ = (
     'BrokerCommunicator',
     'RabbitmqBroker',
-    'WorkerExecutor',
-    'WorkerInfo',
 )
 
 # fmt: on
