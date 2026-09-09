574dbd46c1b9774061ffa6aff24d30a6f00e7453
Alexander Goscinski <alex.go@posteo.de>
2026-09-09 08:08:00 +0200

    ✨ Prepare storage migrations for AiiDA v3 (#7621)
    Add empty v3 migration revisions for the PostgreSQL and archive
    storage backends, matching sqlite_dos. Cover the new revision paths
    and record the unchanged PostgreSQL and archive schema snapshot.

---
+++ src/aiida/storage/sqlite_zip/migrations/versions/main_0002_aiida_v3_0_0.py
@@ -0,0 +1,26 @@
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+"""Prepare the storage schema for AiiDA v3.0.0.
+
+Revision ID: main_0002
+Revises: main_0001
+Create Date: 2026-09-07
+"""
+
+revision = 'main_0002'
+down_revision = 'main_0001'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Migrations for the upgrade."""
+
+
+def downgrade():
+    """Migrations for the downgrade."""
