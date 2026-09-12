48dc5eb1fdb5ba614fca52feb6f28a132dd72a79
Alexander Goscinski <alex.go@posteo.de>
2026-09-12 15:38:13 +0200

    MANUAL PLUMBING
    Ruff isort autofix for atomistic test imports

--- aiida-core/tests/plugins/test_factories.py
+++ aiida-core/tests/plugins/test_factories.py
@@ -17,9 +17,9 @@ from aiida.orm.implementation.storage_backend import StorageBackend
 from aiida.parsers import Parser
 from aiida.plugins import entry_point, factories
 from aiida.schedulers import Scheduler
+from aiida.transports import AsyncTransport, BlockingTransport, Transport
 from aiida_atomistic.tools.data.orbital.orbital import Orbital
 from aiida_atomistic.tools.dbimporters import DbImporter
-from aiida.transports import AsyncTransport, BlockingTransport, Transport
 
 
 def custom_load_entry_point(group, name):
